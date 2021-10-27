90e614fdd0f95a230336e794952b57c34c4d0a5c
Sergei Petrov <040620@mail.ru>
2021-10-27 20:43:37 +0300

    Homework1.8 is done

--- src/sky/pro_lesson_6/ArraysLessonSecond.java
+++ src/sky/pro_lesson_6/ArraysLessonSecond.java
@@ -173,8 +173,8 @@ public class ArraysLessonSecond {
         System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] - arr[j] == target) {
-                    int[] value = {i, j};
+                if (arr[i] + arr[j] == target) {
+                    int[] value = {arr[i], arr[j]};
                     System.out.println(Arrays.toString(value));
                 }
             }
